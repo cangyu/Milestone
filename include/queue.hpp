20e1da991ee3433cb23ef05060e630610c75985e
Yu Cang <yhcy1993@gmail.com>
2016-05-28 22:03:53 +0800

    finish stack and queue

---
+++ include/queue.hpp
@@ -0,0 +1,110 @@
+#ifndef SJTU_QUEUE_HPP
+#define SJTU_QUEUE_HPP
+
+#include "list.hpp"
+#include "exceptions.hpp"
+
+#include <cstdlib>
+#include <algorithm>
+
+namespace sjtu 
+{
+/**
+ * a container whose behavior is similar to a queue.
+ * It should be based on the list you had written before.
+ * It should be based on the list you had written before.
+ * It should be based on the list you had written before.
+ * 重要的事情要说三遍：请调用自己写的list来实现
+ * 重要的事情要说三遍：请调用自己写的list来实现
+ * 重要的事情要说三遍：请调用自己写的list来实现
+ */
+template<typename T>
+class queue 
+{
+private:
+	typedef sjtu::list<T> ContainerTypeDef;
+	
+	ContainerTypeDef *container;
+
+	void swap(queue &rhs)
+	{
+		using std::swap;
+		swap(container, rhs.container);
+	}
+
+public:
+	/**
+	 * constructors
+	 */
+	queue() 
+	{
+		container = (ContainerTypeDef*)malloc(sizeof(ContainerTypeDef));
+		new (container) ContainerTypeDef();
+	}
+	queue(const queue<T> &other) 
+	{
+		container = (ContainerTypeDef*)malloc(sizeof(ContainerTypeDef));
+		new (container) ContainerTypeDef(*(other.container));
+	}
+
+	/**
+	 * deconstructor
+	 */
+	~queue() 
+	{
+		container->~ContainerTypeDef();
+		free(container);
+	}
+
+	/**
+	 * Assignment operator
+	 */
+	queue &operator=(queue other)
+	{
+		swap(other);
+		return *this;
+	}
+	
+	/**
+	 * get the first of the queue.
+	 * @return a reference of the first element.
+	 * throw container_is_empty if empty() returns true;
+	 */
+	const T & front() const 
+	{
+		if (empty())
+			throw container_is_empty();
+
+		return container->front();
+	}
+
+	/**
+	 * push new element to the queue.
+	 */
+	void push(const T &e) { container->push_back(e); }
+
+	/**
+	 * delete the first element.
+	 * throw container_is_empty if empty() returns true;
+	 */
+	void pop()
+	{
+		if (empty())
+			throw container_is_empty();
+		
+		container->pop_front();
+	}
+	/**
+	 * return the number of the elements.
+	 */
+	size_t size() const { return container->size(); }
+
+	/**
+	 * check if the container has at least an element.
+	 * @return true if it is empty, false if it has at least an element.
+	 */
+	bool empty() const { return container->empty(); }
+};
+
+}
+#endif
---
+++ include/stack.hpp
@@ -0,0 +1,103 @@
+#ifndef SJTU_STACK_HPP
+#define SJTU_STACK_HPP
+
+#include "vector.hpp"
+
+#include <cstdlib>
+#include <algorithm>
+
+namespace sjtu 
+{
+/**
+ * a container whose behavior is similar to a stack.
+ * It should be based on the vector and list you had written before.
+ * The default ListType is vector, but you should make sure that 
+ * the stack you design is also correct when ListType is sjtu::list.
+ */
+template<typename T, typename ListType = vector<T>>
+class stack 
+{
+private:
+
+	ListType *container;
+
+	void swap(stack &rhs) { std::swap(container, rhs.container); }
+
+public:
+	/**
+	 *  constructors
+	 */
+	stack() 
+	{
+		container = (ListType*)malloc(sizeof(ListType));
+		new (container) ListType();
+	}
+	stack(const stack &other) 
+	{
+		container = (ListType*)malloc(sizeof(ListType));
+		new (container) ListType(*(other.container));
+	}
+
+	/**
+	 * deconstructor
+	 */
+	~stack()
+	{
+		container->~ListType();
+		free(container);
+	}
+
+	/**
+	 * Assignment operator
+	 */
+	stack &operator=(stack other) 
+	{
+		swap(other);
+		return *this;
+	}
+	
+	/**
+	 * get the top of the stack.
+	 * @return a reference of the top element.
+	 * throw container_is_empty when empty() returns true.
+	 */
+	const T & top() const 
+	{
+		if (empty())
+			throw container_is_empty();
+
+		return container->back(); 
+	}
+
+	/**
+	 * push new element to the stack.
+	 */
+	void push(const T &e) { container->push_back(e); }
+	
+	/**
+	 * delete the top element.
+	 * throw container_is_empty if empty() returns true;
+	 */
+	void pop() 
+	{
+		if (empty())
+			throw container_is_empty();
+
+		container->pop_back(); 
+	}
+
+	/**
+	 * return the number of the elements.
+	 */
+	size_t size() const { return container->size(); }
+
+	/**
+	 * check if the container has at least an element.
+	 * @return true if it is empty, false if it has at least an element.
+	 */
+	bool empty() const { return container->empty(); }
+};
+
+}
+
+#endif

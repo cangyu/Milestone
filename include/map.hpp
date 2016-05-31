15719efbf62c5faa6c57b58892d1dcd6b20b0612
Yu Cang <yhcy1993@gmail.com>
2016-05-31 23:58:55 +0800

    Gave up the 2-layer structure
    I don't think we should put the so called generic programming at the first place, which is too complex to use. The properties of C++ is too bad to use, and I think the purpose or the principle behind of this language is not as beautiful as C.

--- include/map.hpp
+++ include/map.hpp
@@ -7,32 +7,363 @@
 #include "utility.hpp"
 #include "exceptions.hpp"
 
+#include <cstdlib>
+#include <ctime>
+
 namespace sjtu 
 {
 /**
  * a container like std::map
  */
-template<class Key, class T, class Compare = std::less<Key>, class BalanceTree = BST<Key, T, std::select1st<pair<Key, T> >, Compare> >
+template<class Key, class T, class Compare = std::less<Key> >
 class map
 {
-private:
-	Compare *cmp;
-	BalanceTree *bt;
-
 public:
-	/**
-	 * the internal type of data.
-	 */
 	typedef pair<const Key, T> value_type;
+	typedef struct node
+	{
+		node *left, *right, *parent;
+		value_type *data;
 
-	typedef iterator BlanceTree::iterator;
-	typedef const_iterator BalanceTree::const_iterator;
+		node() :left(nullptr), right(nullptr), parent(nullptr), data(nullptr) {}
+		node(const value_type &_d, node *_p = nullptr, node *_l = nullptr, node *_r = nullptr) :left(_l), right(_r), parent(_p), data(new value_type(_d)) {}
+		node(const node &other) :left(other.left), right(other.right), parent(other.parent), data(nullptr)
+		{
+			if (!other.data)
+				data = new T(*other.data);
+		}
+		node &operator=(const node &rhs)
+		{
+			node tmp(rhs);
+			std::swap(left, tmp.left);
+			std::swap(right, tmp.right);
+			std::swap(parent, tmp.parent);
+			std::swap(data, tmp.data);
+			return *this;
+		}
+		~node()
+		{
+			if (!data)
+				delete data;
+			data = nullptr;
+		}
+	}NodeTypeDef, *pNodeTypeDef;
+
+private:
+	pNodeTypeDef root, header;
+	size_t nodeCnt;
+	Compare *cmp;//设成指针形式便于swap
+
+	void swap(map &rhs)
+	{
+		using std::swap;
+		swap(root, rhs.root);
+		swap(header, rhs.header);
+		swap(nodeCnt, rhs.nodeCnt);
+		swap(cmp, rhs.cmp);
+	}
+	void makeEmpty(pNodeTypeDef &_r)
+	{
+		if (!_r)
+			return;
+
+		makeEmpty(_r->left);
+		makeEmpty(_r->right);
+		delete _r;
+	}
+	bool checkAscription(const iterator &it) const	{ return it.identity() == header; }
+	bool checkAscription(const const_iterator &it) const { return it.identity() == header; }
+	void updateLeftMost()
+	{
+		pNodeTypeDef tmp = header->left;
+		while (tmp->left)
+			tmp = tmp->left;
+		header->left = left;
+	}
+	void updateRightMost()
+	{
+		pNodeTypeDef tmp = header->right;
+		while (tmp->right)
+			tmp = tmp->right;
+		header->right = tmp;
+	}
+
+	pair<pNodeTypeDef,bool> insert(const value_type &val, pNodeTypeDef &r, pNodeTypeDef p)
+	{
+		if (!r)
+		{
+			r = new NodeTypeDef(val, p);
+			++nodeCnt;
+			updateLeftMost();
+			updateRightMost();
+			return pair<pNodeTypeDef, bool>(r, true);
+		}
+		else if ((*cmp)(val.first, r->data->first))
+			return insert(val, r->left, r);
+		else if ((*cmp)(r->data->first, val.first))
+			return insert(val, r->right, r);
+		else
+			return pair<pNodeTypeDef, bool>(r, false);
+	}
+	pNodeTypeDef find(pNodeTypeDef r, const Key &k)
+	{
+		if (!r)
+			return header;
+		else if ((*cmp)(k, r->data->first))
+			return find(r->left, k);
+		else if ((*cmp)(r->data->first, k))
+			return find(r->right, k);
+		else
+			return r;
+	}
+	
+public:
+	//def of iterator
+	class const_iterator;
+	class iterator
+	{
+	private:
+		pNodeTypeDef cur, bound;
+
+		void increasePtr()
+		{
+			if (cur->right)
+			{
+				pNodeTypeDef tmp = cur->right;
+				while (tmp->left)
+					tmp = tmp->left;
+				cur = tmp;
+			}
+			else
+			{
+				pNodeTypeDef p = cur->parent;
+				while (p && p->right == cur)
+				{
+					cur = p;
+					p = p->parent;
+				}
+				if (p)
+					cur = p;
+			}
+		}
+
+		void decreasePtr()
+		{
+			if (cur == header)//最后一个
+				cur = header->right;
+			else//不是最后一个，按常规方法找前驱
+			{
+				if (cur->left)
+				{
+					pNodeTypeDef tmp = cur->left;
+					while (tmp->right)
+						tmp = tmp->right;
+					cur = tmp;
+				}
+				else
+				{
+					pNodeTypeDef p = cur->parent;
+					while (p && p->left == cur)
+					{
+						cur = p;
+						p = p->parent;
+					}
+					if (p)
+						cur = p;
+				}
+			}
+		}
+
+	public:
+		iterator() :cur(nullptr),bound(header)	{}
+		iterator(const iterator &other) :cur(other.cur), bound(other.bound)	{}
+
+		/**
+		 * iter++
+		 */
+		iterator operator++(int) 
+		{
+			iterator tmp(*this);
+			increasePtr();
+			return tmp;
+		}
+		/**
+		 *  ++iter
+		 */
+		iterator & operator++() 
+		{
+			increasePtr();
+			return *this;
+		}
+
+		/**
+		 * iter--
+		 */
+		iterator operator--(int) 
+		{
+			iterator tmp(*this);
+			decreasePtr();
+			return tmp;
+		}
+		/**
+		 * --iter
+		 */
+		iterator & operator--() 
+		{
+			decreasePtr();
+			return *this;
+		}
+
+		/**
+		 *  *iter
+		 */
+		value_type& operator*() const { return *cur->data; }
+
+		/**
+		 * an operator to check whether two iterators are same (pointing to the same memory).
+		 */
+		bool operator==(const iterator &rhs) const { return cur == rhs.cur; }
+		bool operator==(const const_iterator &rhs) const { return cur == rhs.cur; }
+		/**
+		 * some other operator for iterator.
+		 */
+		bool operator!=(const iterator &rhs) const { return !operator==(rhs); }
+		bool operator!=(const const_iterator &rhs) const { return !operator==(rhs); }
+
+		/**
+		 * for the support of it->first. 
+		 * See <http://kelvinh.github.io/blog/2013/11/20/overloading-of-member-access-operator-dash-greater-than-symbol-in-cpp/> for help.
+		 */
+		value_type* operator->() const noexcept{ return cur; }
+	};
+	class const_iterator 
+	{
+		// it should has similar member method as iterator.
+		//  and it should be able to construct from an iterator.
+		private:
+			pNodeTypeDef cur, bound;
+
+			void increasePtr()
+			{
+				if (cur->right)
+				{
+					pNodeTypeDef tmp = cur->right;
+					while (tmp->left)
+						tmp = tmp->left;
+					cur = tmp;
+				}
+				else
+				{
+					pNodeTypeDef p = cur->parent;
+					while (p && p->right == cur)
+					{
+						cur = p;
+						p = p->parent;
+					}
+					if (p)
+						cur = p;
+				}
+			}
+
+			void decreasePtr()
+			{
+				if (cur == header)//最后一个
+					cur = header->right;
+				else//不是最后一个，按常规方法找前驱
+				{
+					if (cur->left)
+					{
+						pNodeTypeDef tmp = cur->left;
+						while (tmp->right)
+							tmp = tmp->right;
+						cur = tmp;
+					}
+					else
+					{
+						pNodeTypeDef p = cur->parent;
+						while (p && p->left == cur)
+						{
+							cur = p;
+							p = p->parent;
+						}
+						if (p)
+							cur = p;
+					}
+				}
+			}
+		public:
+			const_iterator() :cur(nullptr), bound(header)	{}
+			const_iterator(const const_iterator &other) :cur(other.cur), bound(other.bound)	{}
+			const_iterator(const iterator &other) :cur(other.cur), bound(other.bound)	{}
+
+			/**
+			* iter++
+			*/
+			const_iterator operator++(int)
+			{
+				const_iterator tmp(*this);
+				increasePtr();
+				return tmp;
+			}
+			/**
+			*  ++iter
+			*/
+			const_iterator & operator++()
+			{
+				increasePtr();
+				return *this;
+			}
+
+			/**
+			* iter--
+			*/
+			const_iterator operator--(int)
+			{
+				const_iterator tmp(*this);
+				decreasePtr();
+				return tmp;
+			}
+			/**
+			* --iter
+			*/
+			const_iterator & operator--()
+			{
+				decreasePtr();
+				return *this;
+			}
+
+			/**
+			*  *iter
+			*/
+			value_type operator*() const { return *cur->data; }
+
+			/**
+			* an operator to check whether two iterators are same (pointing to the same memory).
+			*/
+			bool operator==(const iterator &rhs) const { return cur == rhs.cur; }
+			bool operator==(const const_iterator &rhs) const { return cur == rhs.cur; }
+			/**
+			* some other operator for iterator.
+			*/
+			bool operator!=(const iterator &rhs) const { return !operator==(rhs); }
+			bool operator!=(const const_iterator &rhs) const { return !operator==(rhs); }
+
+			/**
+			* for the support of it->first.
+			* See <http://kelvinh.github.io/blog/2013/11/20/overloading-of-member-access-operator-dash-greater-than-symbol-in-cpp/> for help.
+			*/
+			value_type* operator->() const noexcept{ return cur; }
+	};
 
 	/**
-	 * two constructors
+	 * constructors
 	 */
-	map() :cmp(new Compare()), bt(new BalanceTree(*cmp)) {}
-	map(const map &other) :cmp(new Compare(*other.cmp)), bt(new BalanceTree(*other.bt)) {}
+	map() :root(nullptr), header(new NodeTypeDef()), nodeCnt(0), cmp(new Compare()) {}
+	map(const map &other) :root(nullptr), header(new NodeTypeDef()), nodeCnt(0), cmp(new Compare(other.cmp))
+	{
+		for (auto it = other.cbegin(); it != other.cend(); ++it)
+			insert(*it);
+	}
 	
 	/**
 	 * assignment operator
@@ -40,8 +371,7 @@ public:
 	map & operator=(const map &other) 
 	{
 		map tmp(other);
-		std::swap(cmp, tmp.cmp);
-		std::swap(bt, tmp.bt);
+		swap(rmp);
 		return *this;
 	}
 
@@ -50,8 +380,9 @@ public:
 	 */
 	~map() 
 	{
+		makeEmpty(root);
+		delete header;
 		delete cmp;
-		delete bt;
 	}
 
 	/**
@@ -79,7 +410,7 @@ public:
 	/**
 	 * access specified element 
 	 * Returns a reference to the value that is mapped to a key equivalent to key,
-	 *   performing an insertion if such key does not already exist.
+	 * performing an insertion if such key does not already exist.
 	 */
 	T & operator[](const Key &key) 
 	{
@@ -89,7 +420,6 @@ public:
 		else
 			return insert(pair<Key, T>(key, T()))->second;
 	}
-
 	/**
 	 * behave like at() throw index_out_of_bound if such key does not exist.
 	 */
@@ -103,41 +433,51 @@ public:
 	}
 
 	/**
-	 * return a iterator to the beginning
+	 * return an iterator to the beginning
 	 */
-	iterator begin() { return bt.begin(); }
-	const_iterator cbegin() const { return bt.cbegin(); }
+	iterator begin() { return iterator(header->left); }
+	const_iterator cbegin() const { return const_iterator(header->left); }
 
 	/**
 	 * return a iterator to the end
 	 * in fact, it returns past-the-end.
 	 */
-	iterator end() { return bt.end(); }
-	const_iterator cend() const { return bt.cend(); }
+	iterator end() { return iterator(header); }
+	const_iterator cend() const { return const_iterator(header); }
 
 	/**
 	 * checks whether the container is empty
 	 * return true if empty, otherwise false.
 	 */
-	bool empty() const { return bt.empty(); }
-
+	bool empty() const { return header->left == header && header->right == header; }
+	
 	/**
 	 * returns the number of elements.
 	 */
-	size_t size() const { return bt.size(); }
+	size_t size() const { return nodeCnt; }
 
 	/**
 	 * clears the contents
 	 */
-	void clear() { return bt.clear(); }
+	void clear() 
+	{
+		makeEmpty(root);
+		root = nullptr;
+		header->left = header->right = nullptr;
+		nodeCnt = 0;
+	}
 
 	/**
 	 * insert an element.
 	 * return a pair, the first of the pair is
-	 *   the iterator to the new element (or the element that prevented the insertion), 
-	 *   the second one is true if insert successfully, or false.
+	 * the iterator to the new element (or the element that prevented the insertion), 
+	 * the second one is true if insert successfully, or false.
 	 */
-	pair<iterator, bool> insert(const value_type &value) { return bt.insert(value); }
+	pair<iterator, bool> insert(const value_type &value) 
+	{
+		pair<pNodeTypeDef, bool> ans = insert(value, root, header);
+		return pair<iterator, bool>(iterator(ans.first), ans.second);
+	}
 
 	/**
 	 * erase the element at pos.
@@ -146,10 +486,70 @@ public:
 	 */
 	void erase(iterator pos) 
 	{
-		if (pos == end() || pos.identity() != bt.identity())
+		if (pos == end() || !checkAscription(pos))
 			throw invalid_iterator();
 
-		bt.erase(pos);
+		if (pos->left && pos->right)//有两个儿子
+		{
+			std::srand(std::time(nullptr));//找替身的方法，为了尽量保持平衡，采用随机地方式选取替身节点
+			bool chooseRight = ((rand() % 32767) / 32767.0) > 0.5;
+			if (chooseRight)
+			{
+				pNodeTypeDef tmp = t->right;
+				while (tmp->left)
+					tmp = tmp->left;
+				
+				tmp->parent->left = nullptr;
+				if (pos == pos->parent->left)
+					pos->parent->left = tmp;
+				else
+					pos->parent->right = tmp;
+
+				tmp->left = pos->left;
+				tmp->right = pos->right;
+				tmp->parent = pos->parent;
+
+				delete pos;
+			}
+			else
+			{
+				pNodeTypeDef tmp = t->left;
+				while (tmp->right)
+					tmp = tmp->right;
+
+				tmp->parent->right = nullptr;
+				if (pos == pos->parent->left)
+					pos->parent->left = tmp;
+				else
+					pos->parent->right = tmp;
+
+				tmp->left = pos->left;
+				tmp->right = pos->right;
+				tmp->parent = pos->parent;
+
+				delete pos;
+			}
+		}
+		else if (!pos->left && !pos->right)//没有儿子
+		{
+			if (pos == pos->parent->left)
+				pos->parent->left = nullptr;
+			else
+				pos->parent->right = nullptr;
+
+			delete pos;
+		}
+		else//有一个儿子
+		{
+			pNodeTypeDef tmp = pos->left ? pos->left : pos->right;
+			tmp->parent = pos->parent;
+			if (pos == pos->parent->left)
+				pos->parent->left = tmp;
+			else
+				pos->parent->right = tmp;
+
+			delete pos;
+		}
 	}
 
 	/**
@@ -167,130 +567,9 @@ public:
 	 * Iterator to an element with key equivalent to key.
 	 *   If no such element is found, past-the-end (see end()) iterator is returned.
 	 */
-	iterator find(const Key &key) { return bt.find(key); }
-	const_iterator find(const Key &key) const { return bt.find(key); }
+	iterator find(const Key &key) 	{ return iterator(find(root, key)); }
+	const_iterator find(const Key &key) const { return const_iterator(find(root, key)); }
 };
-
-template<typename Key,typename T,class KeyOfElem, class Compare=std::less<Key> >
-class BST
-{
-public:
-	struct node
-	{
-		node* left, right;
-		T *data;
-
-		node() :left(nullptr), right(nullptr), data(nullptr) {}
-		node(node *l, node *r, const T &d) :left(l), right(r), data(new T(d)) {}
-		node(const node &rhs) :left(rhs.left), right(rhs.right), data(new T(*(rhs.data)){}
-		~node() 
-		{
-			if (!data)
-				delete data; 
-			data = nullptr;
-		}
-	};
-
-protected:
-	const node *nil = node();
-	node *leftmost;
-	node *rightmost;
-	node *root;
-
-	size_t nodeCnt;
-	Compare cmp;
-
-public:
-	BST() :leftmost(nil), rightmost(nil), root(nullptr), nodeCnt(0), cmp(Compare()) {}
-	BST(const Compare &other):leftmost(nil), rightmost(nil), root(nullptr), nodeCnt(0), cmp(other) {}
-	~BST() { clear(); }
-
-	/**
-	* see BidirectionalIterator at CppReference for help.
-	*
-	* if there is anything wrong throw invalid_iterator.
-	*     like it = map.begin(); --it;
-	*       or it = map.end(); ++end();
-	*/
-	class const_iterator;
-	class iterator {
-	private:
-		/**
-		* TODO add data members
-		*   just add whatever you want.
-		*/
-	public:
-		iterator() {
-			// TODO
-		}
-		iterator(const iterator &other) {
-			// TODO
-		}
-		/**
-		* return a new iterator which pointer n-next elements
-		*   even if there are not enough elements, just return the answer.
-		* as well as operator-
-		*/
-		/**
-		* TODO iter++
-		*/
-		iterator operator++(int) {}
-		/**
-		* TODO ++iter
-		*/
-		iterator & operator++() {}
-		/**
-		* TODO iter--
-		*/
-		iterator operator--(int) {}
-		/**
-		* TODO --iter
-		*/
-		iterator & operator--() {}
-		/**
-		* a operator to check whether two iterators are same (pointing to the same memory).
-		*/
-		value_type & operator*() const {}
-		bool operator==(const iterator &rhs) const {}
-		bool operator==(const const_iterator &rhs) const {}
-		/**
-		* some other operator for iterator.
-		*/
-		bool operator!=(const iterator &rhs) const {}
-		bool operator!=(const const_iterator &rhs) const {}
-
-		/**
-		* for the support of it->first.
-		* See <http://kelvinh.github.io/blog/2013/11/20/overloading-of-member-access-operator-dash-greater-than-symbol-in-cpp/> for help.
-		*/
-		value_type* operator->() const noexcept {}
-	};
-	class const_iterator {
-		// it should has similar member method as iterator.
-		//  and it should be able to construct from an iterator.
-	private:
-		// data members.
-	public:
-		const_iterator() {
-			// TODO
-		}
-		const_iterator(const const_iterator &other) {
-			// TODO
-		}
-		const_iterator(const iterator &other) {
-			// TODO
-		}
-		// And other methods in iterator.
-		// And other methods in iterator.
-		// And other methods in iterator.
-	};
-
-
-
-
-};
-
-
 
 }
 

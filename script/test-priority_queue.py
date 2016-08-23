bccea26968812109e26bd5fe7d37dd1b81ae07f3
Yu Cang <yhcy1993@gmail.com>
2016-08-23 16:47:56 +0800

    attempt to use python

--- script/test-priority_queue.py
+++ script/test-priority_queue.py
@@ -2,5 +2,6 @@ import os
 import sys
 import subprocess
 
-testCompile=subprocess.Popen(["g++","-std=c++11","-O2","../test/priority_queue/priority_queue-basic.cc", "-o", "../pq_basic.exe"], stderr="../pq_basic.txt");
+testCompile=subprocess.Popen(["g++","-std=c++11","-O2","../test/priority_queue/priority_queue-basic.cc", "-o", "../pq_basic.exe"])
 testCompile.wait()
+os.system("pause")

df6c4e60cd8226e0b456b0d0a9050f6303faa6f7
Yu Cang <yhcy1993@gmail.com>
2016-08-24 21:18:22 +0800

    simply complete the constructors

--- include/exceptions.hpp
+++ include/exceptions.hpp
@@ -5,38 +5,51 @@
 #include <cstring>
 #include <string>
 
-namespace sjtu {
-
-class exception {
+namespace sjtu 
+{
+class exception
+{
 protected:
 	const std::string variant = "";
 	std::string detail = "";
+
 public:
-	exception() {}
+	exception() :variant(""), detail("") {}
+	exception(std::string var, std::string desc) :variant(var), detail(desc) {}
 	exception(const exception &ec) : variant(ec.variant), detail(ec.detail) {}
-	virtual std::string what() {
+	
+	virtual std::string what() 
+	{
 		return variant + " " + detail;
 	}
 };
 
-/**
- * TODO
- * Please complete them.
- */
-class index_out_of_bound : public exception {
-	/* __________________________ */
+class index_out_of_bound : public exception 
+{
+public:
+	index_out_of_bound() = default;
+	index_out_of_bound(std::string var, std::string desc) :exception(var, desc) {}
 };
 
-class runtime_error : public exception {
-	/* __________________________ */
+class runtime_error : public exception 
+{
+public:
+	runtime_error() = default;
+	runtime_error(std::string var, std::string desc) :exception(var, desc) {}
 };
 
-class invalid_iterator : public exception {
-	/* __________________________ */
+class invalid_iterator : public exception 
+{
+public:
+	invalid_iterator() = default;
+	invalid_iterator(std::string var, std::string desc) :exception(var,desc) {}
 };
 
-class container_is_empty : public exception {
-	/* __________________________ */
+class container_is_empty : public exception
+{
+public:
+	container_is_empty() = default;
+	container_is_empty(std::string var, std::string desc) :exception(var, desc) {}
 };
 }
 
